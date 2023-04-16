bdf43d6caf7daed07719402ce1b1bf7d05387e6d
kalis <kalis30nov@yahoo.com>
2023-04-16 06:14:56 +0530

    Modified the location of copy roboshop.conf

--- mongodb.sh
+++ mongodb.sh
@@ -2,5 +2,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-##sed command to replace the mongod.conf file
+sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
 systemctl restart mongod
