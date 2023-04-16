4b57c1637c4ff8ca0cbee8ede3629dd7c15bdf0d
kalis <kalis30nov@yahoo.com>
2023-04-16 05:54:10 +0530

    Modified the location of copy roboshop.conf

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 yum install nginx -y
 rm -rf /usr/share/nginx/html/*
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl restart nginx
