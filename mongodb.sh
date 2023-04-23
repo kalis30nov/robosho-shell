f73904072a15d57e43fb523d10e8bee052b8bace
kalis <kalis30nov@yahoo.com>
2023-04-23 07:16:35 +0530

    Modified sed command

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
+
+
+sed -i 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
-sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
 systemctl restart mongod
