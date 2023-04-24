a3a83be6554ec2011b8cc38c14b83e0d7dc46b13
kalis <kalis30nov@yahoo.com>
2023-04-24 07:09:27 +0530

    Updated echo mesage for mongo

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,13 @@
-echo -e "\e[36mCopying repo file\e[0m"
+echo -e "\e[36m  >>>>>>>>>> Copying repo file  <<<<<<<<< \e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m >>>>>>>>>> Installing Mongo package <<<<<<<<< \e[0m"
 yum install mongodb-org -y
 
-
+echo -e "\e[36m >>>>>>>>>> Update IP in config file <<<<<<<<< \e[0m"
 sed -i 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[36m >>>>>>>>>> Enable service  <<<<<<<<< \e[0m"
 systemctl enable mongod
+
+echo -e "\e[36m >>>>>>>>>> Restart service <<<<<<<<< \e[0m"
 systemctl restart mongod
