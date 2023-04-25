7f5451228f5a7729a7864c6823c80fa51d767a81
kalis <kalis30nov@yahoo.com>
2023-04-25 07:36:24 +0530

    update

--- catalouge.sh
+++ catalouge.sh
@@ -1,7 +1,7 @@
 script=$(realpath $0)
 script_path=$(dirname $script)
-echo $script_path 
 source ${script_path}/common.sh
+echo $source 
 exit
 
 echo -e "\e[36m>>>>>>>>>>>>>> Configuring Node JS repo <<<<<<<<<<<<<<<<<\e[0m"
