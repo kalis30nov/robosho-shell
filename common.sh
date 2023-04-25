ac11bfad9b65fbe1fbca3707b3cbf2fd28bb7132
kalis <kalis30nov@yahoo.com>
2023-04-25 16:08:41 +0530

    redirection

--- common.sh
+++ common.sh
@@ -1,4 +1,5 @@
 app_user=roboshop
+log_file=/tmp/roboshop.log
 
 func_title_print () {
 echo -e "\e[36m>>>>>>>>>>>>>> $1 <<<<<<<<<<<<<<<<<\e[0m"
@@ -9,6 +10,7 @@ if [ $1 -eq 0 ]; then
     echo "\e[32mSUCCESS\e0m"
 else
   echo "\e[31mFAILURE\e0m"
+  echo " Check log file $log_file"
   exit 1
 fi
 }
@@ -16,24 +18,24 @@ fi
 func_schema(){
     if [ "$schema_setup" =="mongo" ]; then
         func_title_print " Copy Mongo Repo "
-        rm -rf /etc/yum.repos.d/mongo.repo
-        cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+        rm -rf /etc/yum.repos.d/mongo.repo &>>$log_file
+        cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
         func_exit_status $?
 
         func_title_print " Install Mongo Client "
-        yum install mongodb-org-shell -y
+        yum install mongodb-org-shell -y &>>$log_file
         func_exit_status $?
 
         func_title_print " Configure Mongo Schema "
-        mongo --host mongodb-dev.kalis30nov.online </app/schema/${component}.js
+        mongo --host mongodb-dev.kalis30nov.online </app/schema/${component}.js &>>$log_file
         func_exit_status $?
     fi
     if [ "$schema_setup" =="mysql" ]; then
         func_title_print  "Install MySQL clent"
-        yum install mysql -y
+        yum install mysql -y &>>$log_file
         func_exit_status $?
         func_title_print  "Load Schema"
-        mysql -h mysql-dev.kalis30nov.online -uroot -p${MYSQL_ROOT_PASSWD} < /app/schema/shipping.sql
+        mysql -h mysql-dev.kalis30nov.online -uroot -p${MYSQL_ROOT_PASSWD} < /app/schema/shipping.sql &>>$log_file
         func_exit_status $?
     fi
 }
@@ -41,21 +43,21 @@ func_schema(){
 fun_user_prereq(){
 
         func_title_print  "Adding App User "
-        useradd ${app_user}
+        useradd ${app_user} &>>$log_file
         func_exit_status $?
 
         func_title_print  "Creating App User Homedir "
-        rm -rf /app
+        rm -rf /app &>>$log_file
         mkdir /app
 
         func_title_print  "Download Install ZIP file for App"
-        curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-        ls -l /tmp/${component}.zip
+        curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
+        ls -l /tmp/${component}.zip &>>$log_file
         func_exit_status $?
 
         func_title_print  "Unzip App content "
         cd /app
-        unzip /tmp/${component}.zip
+        unzip /tmp/${component}.zip &>>$log_file
         func_exit_status $?
 }
 
@@ -63,31 +65,31 @@ func_service_systemd() {
 
         func_title_print  "Copy Service"
         rm -rf /etc/systemd/system/${component}.service
-        cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+        cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
         func_exit_status $?
 
         func_title_print  "Restart service"
-        systemctl daemon-reload
-        systemctl enable ${component}
-        systemctl start ${component}
+        systemctl daemon-reload &>>$log_file
+        systemctl enable ${component} &>>$log_file
+        systemctl start ${component} &>>$log_file
         func_exit_status $?
 
 }
 
 func_nodejs() {
         func_title_print "Configuring Node JS repo"
-        curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+        curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
         func_exit_status $?
 
 
         func_title_print  "Install Node Js "
-        yum install nodejs -y
+        yum install nodejs -y &>>$log_file
         func_exit_status $?
 
         fun_user_prereq
 
         func_title_print  "Install dependent files for App "
-        npm install
+        npm install &>>$log_file
         func_exit_status $?
 
         func_schema
@@ -99,13 +101,13 @@ func_nodejs() {
 func_java_install() {
 
         func_title_print  "Installing Maven"
-        yum install maven -y
+        yum install maven -y &>>$log_file
         func_exit_status $?
 
         fun_user_prereq
 
         func_title_print  "Clean Package"
-        mvn clean package
+        mvn clean package &>>$log_file
         mv target/${component}-1.0.jar ${component}.jar
         func_exit_status $?
 
