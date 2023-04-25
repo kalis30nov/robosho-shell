8d803b94adaa4973651056aa001bc274f80428ec
kalis <kalis30nov@yahoo.com>
2023-04-25 14:30:16 +0530

    update

--- common.sh
+++ common.sh
@@ -16,46 +16,79 @@ func_schema(){
         func_title_print " Configure Mongo Schema "
         mongo --host mongodb-dev.kalis30nov.online </app/schema/${component}.js
     fi
+    if [ "$schema_setup" =="mysql" ]; then
+        func_title_print  "Install MySQL clent"
+        yum install mysql -y
+
+        func_title_print  "Load Schema"
+        mysql -h mysql-dev.kalis30nov.online -uroot -p${MYSQL_ROOT_PASSWD} < /app/schema/shipping.sql
+    fi
+}
+
+fun_user_prereq(){
+
+        func_title_print  "Adding App User "
+        useradd ${app_user}
+
+        func_title_print  "Creating App User Homedir "
+        rm -rf /app
+        mkdir /app
+
+        func_title_print  "Download Install ZIP file for App"
+        curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+        ls -l /tmp/${component}.zip
+
+        func_title_print  "Unzip App content "
+        cd /app
+        unzip /tmp/${component}.zip
+}
+
+func_service_systemd() {
+
+        func_title_print  "Copy Service"
+        rm -rf /etc/systemd/system/${component}.service
+        cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+        func_title_print  "Restart service"
+        systemctl daemon-reload
+        systemctl enable ${component}
+        systemctl start ${component}
+
 }
 
 func_nodejs() {
-func_title_print "Configuring Node JS repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+        func_title_print "Configuring Node JS repo"
+        curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 
-func_title_print  "Install Node Js "
-yum install nodejs -y
+        func_title_print  "Install Node Js "
+        yum install nodejs -y
 
-func_title_print  "Adding App User "
-useradd ${app_user}
+        fun_user_prereq
 
-func_title_print  "Creating App User Homedir "
-rm -rf /app
-mkdir /app
+        func_title_print  "Install dependent files for App "
+        npm install
 
-func_title_print  "Download Install ZIP file for App"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-ls -l /tmp/${component}.zip
+        func_schema
 
-func_title_print  "Unzip App content "
-cd /app
-unzip /tmp/${component}.zip
-
-func_title_print  "Install dependent files for App "
-npm install
-
-func_title_print  "Copy ${component} service"
-rm -rf /etc/systemd/system/${component}.service
-cp /root/roboshop-shell/${component}.service /etc/systemd/system/${component}.service
+        func_service_systemd
+}
 
 
-func_title_print  "Start ${component} service "
-systemctl daemon-reload
-systemctl enable ${component}
-systemctl start ${component}
+func_java_install() {
 
-if [${component} ==]
+        func_title_print  "Installing Maven"
+        yum install maven -y
 
-func_schema
+        fun_user_prereq
+
+        func_title_print  "Clean Package"
+        mvn clean package
+        mv target/${component}-1.0.jar ${component}.jar
+
+        func_schema
+
+        func_service_systemd
+
 }
 
--- shipping.sh
+++ shipping.sh
@@ -8,18 +8,6 @@ if [ -z "$MYSQL_ROOT_PASSWD"]; then
   exit
 fi
 
-yum install maven -y
-cp shipping.service /etc/systemd/system/shipping.service
-useradd $app_user
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
-yum install mysql -y
-mysql -h mysql-dev.kalis30nov.online -uroot -p${MYSQL_ROOT_PASSWD} < /app/schema/shipping.sql
-systemctl restart shipping
+component=shipping
+schema_setup=mysql
+func_java_install
