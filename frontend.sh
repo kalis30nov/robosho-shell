87b6d756d6823427e4457cca62d0e6217a481bc2
kalis <kalis30nov@yahoo.com>
2023-05-14 08:23:04 +0530

    update

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,11 @@ func_title_print "Install Ngninx "
 yum install nginx -y &>>$log_file
 func_exit_status $?
 
+func_title_print "Start Nginx "
+systemctl enable nginx &>>$log_file
+systemctl start nginx &>>$log_file
+func_exit_status $?
+
 func_title_print "Remove old config file "
 rm -rf /usr/share/nginx/html/* &>>$log_file
 func_exit_status $?
@@ -23,7 +28,6 @@ cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
 func_exit_status $?
 
-func_title_print "Start Nginx "
-systemctl enable nginx &>>$log_file
+func_title_print "Restart Nginx "
 systemctl restart nginx &>>$log_file
 func_exit_status $?
